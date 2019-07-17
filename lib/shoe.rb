7fcca42870696ffaf361ad8635face84a5ef3008
Jason Lewis <jasonedlewis@gmail.com>
2019-07-17 20:14:26 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 # Make your shoe class here!
 class Shoe
-  
+  attr_accessor :color, :size, :material, :condition
   #SETTERS
   
   def initialize(shoe_arg)
@@ -8,21 +8,21 @@ class Shoe
     
   end
   
-  def color=(color)
-    @color= color
-  end
+  # def color=(color)
+  #   @color= color
+  # end
   
-  def size=(size)
-    @size = 9.5
-  end
+  # def size=(size)
+  #   @size = size
+  # end
   
-  def material=(material)
-    @material= material
-  end
+  # def material=(material)
+  #   @material= material
+  # end
   
-  def condition=(condition)
-    @condition = condition
-  end
+  # def condition=(condition)
+  #   @condition = condition
+  # end
   
   def cobble
     puts "Your shoe is as good as new!"
@@ -33,26 +33,26 @@ class Shoe
   
   #GETTERS
   
-  def brand
-    @brand
-  end
+  # def brand
+  #   @brand
+  # end
   
   
-  def color
-    @color
-  end
+  # def color
+  #   @color
+  # end
   
-  def size
-    @size
-  end
+  # def size
+  #   @size
+  # end
   
-  def material
-    @material
-  end
+  # def material
+  #   @material
+  # end
   
-  def condition
-    @condition
-  end
+  # def condition
+  #   @condition
+  # end
   
   
   # def cobble
